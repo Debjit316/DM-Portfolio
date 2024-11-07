33202dd9544fd9b2c973c23a3a035d0fc00189a7
Debjit Mukhopadhyay <debjitmukhopadhyay316@gmail.com>
2024-11-07 23:33:27 +0530

    ripple removed

--- src/App.jsx
+++ src/App.jsx
@@ -15,12 +15,11 @@ const App = () => {
       <div className="fixed top-0 -z-10 h-full w-full "></div>
       <div class="absolute top-0 z-[-2] min-h-screen w-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
         <div className="container mx-auto px-20">
+          {/* <Ripple /> */}
           <Navbar />
           <Hero />
-          <Ripple />
           <About />
           <Skills />
-          <Ripple />
           <Education />
           <Projects />
           <Contacts />
